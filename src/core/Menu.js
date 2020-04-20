e774767d2ad302e039133be23ab5f643e11806ff
Mayur <mayur.jaganmohan@gmail.com>
2020-04-20 23:13:04 +0530

    Section 15 bugfix commit not working

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import { signout, isAutheticated } from "../auth/helper";
 
 const currentTab = (history, path) => {
   if (history.location.pathname === path) {
@@ -63,15 +64,27 @@ const Menu = ({ history }) => (
           Signin
         </Link>
       </li>
-      <li className="nav-item">
-        <Link
-          style={currentTab(history, "/signout")}
-          className="nav-link"
-          to="/signout"
-        >
-          Singnout
-        </Link>
-      </li>
+      {isAutheticated() && (
+        <li className="nav-item">
+          {/* <Link
+            style={currentTab(history, "/signout")}
+            className="nav-link"
+            to="/signout"
+          >
+            Singnout
+          </Link> */}
+          <span
+            className="nav-link text-warning"
+            onClick={() => {
+              signout(() => {
+                history.push("/");
+              });
+            }}
+          >
+            Signout
+          </span>
+        </li>
+      )}
     </ul>
   </div>
 );
