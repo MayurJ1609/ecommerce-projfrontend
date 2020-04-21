7624a6e3dddd20c20c8e6f41e701e0a7e047173e
Mayur <mayur.jaganmohan@gmail.com>
2020-04-21 23:57:07 +0530

    Admin dashboard, user dashboard commit

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./core/Home";
 import SignUp from "./user/Signup";
 import SignIn from "./user/Signin";
+import AdminRoute from "./auth/helper/AdminRoutes";
+import PrivateRoute from "./auth/helper/PrivateRoutes";
+import UserDashboard from "./user/UserDashBoard";
+import AdminDashboard from "./user/AdminDashBoard";
 
 const Routes = () => {
   return (
@@ -11,6 +15,16 @@ const Routes = () => {
         <Route path="/" exact component={Home} />
         <Route path="/signup" exact component={SignUp} />
         <Route path="/signin" exact component={SignIn} />
+        <PrivateRoute
+          path="/user/dashboard"
+          exact
+          component={UserDashboard}
+        ></PrivateRoute>
+        <PrivateRoute
+          path="/admin/dashboard"
+          exact
+          component={AdminDashboard}
+        ></PrivateRoute>
       </Switch>
     </BrowserRouter>
   );
