328964c429209af6aef4006cfc2a8b0540f4046d
Mayur <mayur.jaganmohan@gmail.com>
2020-04-28 22:38:11 +0530

    Section 17 - reusable component card.js

---
+++ src/core/card.js
@@ -0,0 +1,53 @@
+import React from "react";
+import ImageHelper from "./helper/ImageHelper";
+
+const Card = ({ product, addToCart = true, removeFromCart = false }) => {
+  const cardTitle = product ? product.name : "A Photo from pexcel";
+  const cardDescription = product ? product.description : "Default description";
+  const cardprice = product ? product.price : "default";
+
+  const showAddToCart = () => {
+    return (
+      addToCart && (
+        <button
+          onClick={() => {}}
+          className="btn btn-block btn-outline-success mt-2 mb-2"
+        >
+          Add to Cart
+        </button>
+      )
+    );
+  };
+
+  const showRemoveFromCart = () => {
+    return (
+      removeFromCart && (
+        <button
+          onClick={() => {}}
+          className="btn btn-block btn-outline-danger mt-2 mb-2"
+        >
+          Remove from cart
+        </button>
+      )
+    );
+  };
+
+  return (
+    <div className="card text-white bg-dark border border-info ">
+      <div className="card-header lead">{cardTitle}</div>
+      <div className="card-body">
+        <ImageHelper product={product} />
+        <p className="lead bg-success font-weight-normal text-wrap">
+          {cardDescription}
+        </p>
+        <p className="btn btn-success rounded  btn-sm px-4">$ {cardprice}</p>
+        <div className="row">
+          <div className="col-12">{showAddToCart()}</div>
+          <div className="col-12">{showRemoveFromCart()}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
