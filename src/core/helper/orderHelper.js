031920b7b850073612b97c87ce53c58fafc3c6db
Mayur <mayur.jaganmohan@gmail.com>
2020-05-06 23:48:33 +0530

    Fixed issue of pushing order to user

--- src/core/helper/orderHelper.js
+++ src/core/helper/orderHelper.js
@@ -6,7 +6,7 @@ export const createOrder = (userId, token, orderData) => {
 		method: 'POST',
 		headers: {
 			Accept: 'application/json',
-			'Content-Type': 'application / json',
+			'Content-Type': 'application/json',
 			Authorization: `Bearer ${token}`,
 		},
 		body: JSON.stringify({ order: orderData }),
