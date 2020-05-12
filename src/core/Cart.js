4975e9465e622c292f556ce67a8dd73e87a26f95
Mayur <mayur.jaganmohan@gmail.com>
2020-05-12 20:17:09 +0530

    Changes for Cart.js for debugging product

--- src/core/Cart.js
+++ src/core/Cart.js
@@ -1,61 +1,61 @@
-import React, { useState, useEffect } from "react";
-import "../styles.css";
-import { API } from "../backend";
-import Base from "./Base";
-import Card from "./Card";
-import { loadCart } from "./helper/cartHelper";
-import PaymentB from "./PaymentB";
+import React, { useState, useEffect } from 'react';
+import '../styles.css';
+import { API } from '../backend';
+import Base from './Base';
+import Card from './Card';
+import { loadCart } from './helper/cartHelper';
+import PaymentB from './PaymentB';
 
 const Cart = () => {
-  const [products, setProducts] = useState([]);
-  const [reload, setReload] = useState(false);
+	const [products, setProducts] = useState([]);
+	const [reload, setReload] = useState(false);
 
-  useEffect(() => {
-    setProducts(loadCart());
-  }, [reload]);
+	useEffect(() => {
+		setProducts(loadCart());
+	}, [reload]);
 
-  const loadAllProducts = (products) => {
-    return (
-      <div>
-        <h2>This section is to load products</h2>
-        {products.map((product, index) => (
-          <Card
-            key={index}
-            product={product}
-            addtocart={false}
-            removefromcart={true}
-            setReload={setReload}
-            reload={reload}
-          />
-        ))}
-      </div>
-    );
-  };
+	const loadAllProducts = (products) => {
+		return (
+			<div>
+				<h2>This section is to load products</h2>
+				{products.map((product, index) => (
+					<Card
+						key={index}
+						product={product}
+						addtocart={false}
+						removefromcart={true}
+						setReload={setReload}
+						reload={reload}
+					/>
+				))}
+			</div>
+		);
+	};
 
-  const loadCheckout = () => {
-    return (
-      <div>
-        <h2>This section is to For checkout</h2>
-      </div>
-    );
-  };
+	const loadCheckout = () => {
+		return (
+			<div>
+				<h2>This section is to For checkout</h2>
+			</div>
+		);
+	};
 
-  return (
-    <Base title="Cart page" description="Ready to check out">
-      <div className="row text-center">
-        <div className="col-6">
-          {products.length > 0 ? (
-            loadAllProducts(products)
-          ) : (
-            <h3>No products in cart</h3>
-          )}
-        </div>
-        <div className="col-6">
-          <PaymentB products={products} setReload={setReload} />
-        </div>
-      </div>
-    </Base>
-  );
+	return (
+		<Base title="Cart page" description="Ready to check out">
+			<div className="row text-center">
+				<div className="col-6">
+					{products.length > 0 ? (
+						loadAllProducts(products)
+					) : (
+						<h3>No products in cart</h3>
+					)}
+				</div>
+				<div className="col-6">
+					<PaymentB products={products} setReload={setReload} />
+				</div>
+			</div>
+		</Base>
+	);
 };
 
 export default Cart;
