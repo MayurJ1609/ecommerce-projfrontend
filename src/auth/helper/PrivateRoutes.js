9756dd3ac173f0377e26b94cc187e0a53d897764
Mayur <mayur.jaganmohan@gmail.com>
2020-05-18 16:22:55 +0530

    Uderstanding {..rest}, Component, {...props}

--- src/auth/helper/PrivateRoutes.js
+++ src/auth/helper/PrivateRoutes.js
@@ -1,25 +1,29 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import { isAutheticated } from "./index";
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { isAutheticated } from './index';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        isAutheticated() ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/signin",
-              state: { from: props.location },
-            }}
-          />
-        )
-      }
-    />
-  );
+	console.log('Understanding ...rest : ' + JSON.stringify(rest));
+	//{"path":"/user/dashboard","exact":true,"location":{"pathname":"/user/dashboard","search":"","hash":""},"computedMatch":{"path":"/user/dashboard","url":"/user/dashboard","isExact":true,"params":{}}}
+	return (
+		<Route
+			{...rest} // understand here
+			render={(
+				props //understand props
+			) =>
+				isAutheticated() ? (
+					<Component {...props} />
+				) : (
+					<Redirect
+						to={{
+							pathname: '/signin',
+							state: { from: props.location },
+						}}
+					/>
+				)
+			}
+		/>
+	);
 };
 
 export default PrivateRoute;
