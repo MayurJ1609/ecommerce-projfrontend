927ccc859f9a26d40d2853157b4fefffc145f05c
Mayur <mayur.jaganmohan@gmail.com>
2020-05-29 19:16:01 +0530

    Modifying for some understanding

--- src/Routes.js
+++ src/Routes.js
@@ -1,71 +1,71 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./core/Home";
-import SignUp from "./user/Signup";
-import SignIn from "./user/Signin";
-import AdminRoute from "./auth/helper/AdminRoutes";
-import PrivateRoute from "./auth/helper/PrivateRoutes";
-import UserDashboard from "./user/UserDashBoard";
-import AdminDashboard from "./user/AdminDashBoard";
-import AddCategory from "./admin/AddCategory";
-import ManageCategories from "./admin/ManageCategories";
-import AddProduct from "./admin/AddProduct";
-import ManageProducts from "./admin/ManageProducts";
-import UpdateProduct from "./admin/UpdateProduct";
-import UpdateCategory from "./admin/UpdateCategory";
-import Cart from "./core/Cart";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './core/Home';
+import SignUp from './user/Signup';
+import SignIn from './user/Signin';
+import AdminRoute from './auth/helper/AdminRoutes';
+import PrivateRoute from './auth/helper/PrivateRoutes';
+import UserDashboard from './user/UserDashBoard';
+import AdminDashboard from './user/AdminDashBoard';
+import AddCategory from './admin/AddCategory';
+import ManageCategories from './admin/ManageCategories';
+import AddProduct from './admin/AddProduct';
+import ManageProducts from './admin/ManageProducts';
+import UpdateProduct from './admin/UpdateProduct';
+import UpdateCategory from './admin/UpdateCategory';
+import Cart from './core/Cart';
 
 const Routes = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/signup" exact component={SignUp} />
-        <Route path="/signin" exact component={SignIn} />
-        <Route path="/cart" exact component={Cart} />
-        <PrivateRoute
-          path="/user/dashboard"
-          exact
-          component={UserDashboard}
-        ></PrivateRoute>
-        <AdminRoute
-          path="/admin/dashboard"
-          exact
-          component={AdminDashboard}
-        ></AdminRoute>
-        <AdminRoute
-          path="/admin/create/category"
-          exact
-          component={AddCategory}
-        ></AdminRoute>
-        <AdminRoute
-          path="/admin/categories"
-          exact
-          component={ManageCategories}
-        ></AdminRoute>
-        <AdminRoute
-          path="/admin/create/product"
-          exact
-          component={AddProduct}
-        ></AdminRoute>
-        <AdminRoute
-          path="/admin/products"
-          exact
-          component={ManageProducts}
-        ></AdminRoute>
-        <AdminRoute
-          path="/admin/product/update/:productId"
-          exact
-          component={UpdateProduct}
-        ></AdminRoute>
-        <AdminRoute
-          path="/admin/category/update/:categoryId"
-          exact
-          component={UpdateCategory}
-        ></AdminRoute>
-      </Switch>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/signup" exact component={SignUp} />
+				<Route path="/signin" exact component={SignIn} />
+				<Route path="/cart" exact component={Cart} />
+				<PrivateRoute
+					path="/user/dashboard"
+					exact
+					component={UserDashboard}
+				></PrivateRoute>
+				<AdminRoute
+					path="/admin/dashboard"
+					exact
+					component={AdminDashboard}
+				></AdminRoute>
+				<AdminRoute
+					path="/admin/create/category"
+					exact
+					component={AddCategory}
+				></AdminRoute>
+				<AdminRoute
+					path="/admin/categories"
+					exact
+					component={ManageCategories}
+				></AdminRoute>
+				<AdminRoute
+					path="/admin/create/product"
+					exact
+					component={AddProduct}
+				></AdminRoute>
+				<AdminRoute
+					path="/admin/products"
+					exact
+					component={ManageProducts}
+				></AdminRoute>
+				<AdminRoute
+					path="/admin/product/update/:productId"
+					exact
+					component={UpdateProduct}
+				></AdminRoute>
+				<AdminRoute
+					path="/admin/category/update/:categoryId"
+					exact
+					component={UpdateCategory}
+				></AdminRoute>
+			</Switch>
+		</BrowserRouter>
+	);
 };
 
 export default Routes;
